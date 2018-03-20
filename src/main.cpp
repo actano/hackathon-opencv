6026e7b94d8175bbc572547ecd8ba83076ebdadc
Christoph Vitz <c.vitz@itsonix.eu>
2018-03-20 13:32:42 +0100

    Approximate shapes and extract drawing function

--- src/main.cpp
+++ src/main.cpp
@@ -40,15 +40,27 @@ int main( int, char** argv )
     imshow( "Drawing", drawing );
 
     for(const vector<Point>& contour: contours) {
-        cout<<"Begin Contour"<<endl;
-        for(const Point& point: contour) {
+        cout<<"Begin Poly"<<endl;
+        vector<Point> poly;
+        approxPolyDP(contour, poly, 50, true);
+        for(const Point& point: poly) {
             cout<<point<<endl;
         }
-        cout<<"End Contour"<<endl;
+        cout<<"End Poly"<<endl;
     }
 
     waitKey(0);
     return(0);
+}
+
+void draw_contour(const vector<vector<Point>> &contours, Size size, OutputArray out) {
+    Mat drawing = Mat::zeros( size, CV_8UC3 );
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours, (int)i, color, 2, 8);
+    }
+    out.assign(drawing);
 }
 
 vector<vector<Point>> recognise_shape(InputArray in, OutputArray out)
@@ -56,14 +68,6 @@ vector<vector<Point>> recognise_shape(InputArray in, OutputArray out)
     vector<Vec4i> hierarchy;
     vector<vector<Point>> contours;
     findContours(in.getMat(), contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-    Mat drawing = Mat::zeros( in.size(), CV_8UC3 );
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
-    }
-    out.assign(drawing);
-
+    draw_contour(contours, in.size(), out);
     return contours;
 }
